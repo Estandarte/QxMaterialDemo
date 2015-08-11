5adb86414c260e796ffae2a4c650e2e1e06fbe72
Leandro <leandro.chaves@h2asol.com>
2015-08-11 14:06:39 -0300

    Created demo project.

---
+++ source/class/qxmaterialdemo/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("qxmaterialdemo.theme.Theme",
+{
+  meta :
+  {
+    color : qxmaterialdemo.theme.Color,
+    decoration : qxmaterialdemo.theme.Decoration,
+    font : qxmaterialdemo.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : qxmaterialdemo.theme.Appearance
+  }
+});
