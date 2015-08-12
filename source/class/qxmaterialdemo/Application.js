5a04d2f405206bc4434e20dd6bc257b86056bbe3
Leandro <leandro.chaves@h2asol.com>
2015-08-12 13:18:20 -0300

    Added Google MDL script/css/icons.

--- source/class/qxmaterialdemo/Application.js
+++ source/class/qxmaterialdemo/Application.js
@@ -1,71 +1,43 @@
-/* ************************************************************************
-
-   Copyright:
-
-   License:
-
-   Authors:
-
-************************************************************************ */
-
 /**
- * This is the main application class of your custom application "QxMaterialDemo"
+ * Main application class of "QxMaterialDemo".
  *
  * @asset(qxmaterialdemo/*)
  */
-qx.Class.define("qxmaterialdemo.Application",
-{
-  extend : qx.application.Standalone,
+qx.Class.define("qxmaterialdemo.Application", {
+    extend: qx.application.Standalone,
+    members: {
+        /**
+         * This method contains the initial application code and gets called
+         * during startup of the application
+         *
+         * @lint ignoreDeprecated(alert)
+         */
+        main: function() {
+            // Call super class
+            this.base(arguments);
 
+            // Enable logging in debug variant
+            if (qx.core.Environment.get("qx.debug")) {
+                // support native logging capabilities, e.g. Firebug for Firefox
+                qx.log.appender.Native;
+            }
 
+            // Create a button
+            var button1 = new qx.ui.form.Button("First Button");
 
-  /*
-  *****************************************************************************
-     MEMBERS
-  *****************************************************************************
-  */
+            // Document is the application root
+            var doc = this.getRoot();
 
-  members :
-  {
-    /**
-     * This method contains the initial application code and gets called 
-     * during startup of the application
-     * 
-     * @lint ignoreDeprecated(alert)
-     */
-    main : function()
-    {
-      // Call super class
-      this.base(arguments);
+            // Add button to document at fixed coordinates
+            doc.add(button1, {
+                left: 100,
+                top: 50
+            });
 
-      // Enable logging in debug variant
-      if (qx.core.Environment.get("qx.debug"))
-      {
-        // support native logging capabilities, e.g. Firebug for Firefox
-        qx.log.appender.Native;
-        // support additional cross-browser console. Press F7 to toggle visibility
-        qx.log.appender.Console;
-      }
-
-      /*
-      -------------------------------------------------------------------------
-        Below is your actual application code...
-      -------------------------------------------------------------------------
-      */
-
-      // Create a button
-      var button1 = new qx.ui.form.Button("First Button", "qxmaterialdemo/test.png");
-
-      // Document is the application root
-      var doc = this.getRoot();
-
-      // Add button to document at fixed coordinates
-      doc.add(button1, {left: 100, top: 50});
-
-      // Add an event listener
-      button1.addListener("execute", function(e) {
-        alert("Hello World!");
-      });
+            // Add an event listener
+            button1.addListener("execute", function(e) {
+                alert("Hello World!");
+            });
+        }
     }
-  }
 });
